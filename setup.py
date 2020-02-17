19be6f07a9d64926923d16d21f22c625e11c6343
jchacks <chacksfieldj@gmail.com>
2020-02-17 11:16:13 +0100

    Package setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='robots',
+    version='0.1',
+    description='Robocode python clone',
+    url='https://github.com/jchacks/robots',
+    author='jchacks',
+    # author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=['robots'],
+    zip_safe=False
+)
