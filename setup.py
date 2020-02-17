3749462c84f542c6db4cb48b6c861c12326a6766
jchacks <chacksfieldj@gmail.com>
2020-02-17 11:24:15 +0100

    Added README.md to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='robots',
     version='0.1',
     description='Robocode python clone',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/jchacks/robots',
     author='jchacks',
     # author_email='flyingcircus@example.com',
-    license='MIT',
+    # license='MIT',
     packages=['robots'],
+    install_requires=[
+        'pygame',
+        'numpy'
+    ],
+    include_package_data=True,
     zip_safe=False
 )
