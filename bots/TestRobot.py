2c57e66d28c0b498e929a0cc4ed5f350454ce90c
jchacks <chacksfieldj@gmail.com>
2020-02-18 20:44:54 +0100

    Removed constraints on TestRobot

--- bots/TestRobot.py
+++ bots/TestRobot.py
@@ -6,19 +6,16 @@ from robots import AdvancedRobot
 class TestRobot(AdvancedRobot):
     def on_init(self):
         super(TestRobot, self).on_init()
-        self.set_max_rotation(1)
         self.radar.locked = True
         self.gun.locked = True
         self.scans = []
 
     def do(self, tick):
         self.turn_left(360)
-        self.move_forward(1)
 
     def on_scanned_robot(self, scanned):
         self.scans.append((self.position, scanned))
         self.fire(3)
-        print(self.scans)
 
     def draw(self, surface):
         for pos, scan in self.scans:
