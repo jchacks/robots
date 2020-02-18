39a0202f0563393b865f1e2dbab964f9b118dd13
jchacks <chacksfieldj@gmail.com>
2020-02-18 20:00:28 +0100

    Added AiRobot

---
+++ bots/AiRobot.py
@@ -0,0 +1,13 @@
+from robots import SignalRobot
+
+class AiRobot(SignalRobot):
+    def on_init(self):
+        pass
+
+    def do(self, tick):
+        # Call model
+
+        # Apply changes turing etc...
+        pass
+
+    def on_scanned_robot(self, event: ScannedRobotEvent):
