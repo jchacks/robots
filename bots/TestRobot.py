84f3dedfd9d4572077504896c93daf7c6dae5100
jchacks <chacksfieldj@gmail.com>
2020-02-18 20:01:34 +0100

    Propagate tick through to do

--- bots/TestRobot.py
+++ bots/TestRobot.py
@@ -6,12 +6,12 @@ from robots import AdvancedRobot
 class TestRobot(AdvancedRobot):
     def on_init(self):
         super(TestRobot, self).on_init()
-        self.set_max_rotation(5)
+        self.set_max_rotation(1)
         self.radar.locked = True
         self.gun.locked = True
         self.scans = []
 
-    def do(self):
+    def do(self, tick):
         self.turn_left(360)
         self.move_forward(1)
 
