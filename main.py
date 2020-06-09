68113a6f87d30b1aa5ad484fed9b999b85e7d50a
jchacks <chacksfieldj@gmail.com>
2020-06-09 13:29:42 +0200

    Updated README and docs/images

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from robots.app import App
+from robots.app import App, Battle
+from robots.bots import MyFirstRobot, RandomRobot
 
 app = App((600, 400))
-app.set_sim_rate(1)
+app.battle = Battle(app, (600,400), [
+    MyFirstRobot.MyFirstRobot,
+    RandomRobot.RandomRobot
+])
+
+app.set_sim_rate(10)
 app.on_execute()
