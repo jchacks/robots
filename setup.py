aaaead17a008e2dd71ae7631cf4e408367e343ab
Joshua Chacksfield <chacksfieldj@gmail.com>
2021-09-10 09:36:29 +0200

    Removed C code to separate repo

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from os import path
 from setuptools import setup
-from Cython.Build import cythonize
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -17,8 +16,7 @@ setup(
     author="jchacks",
     # license='MIT',
     packages=["robots"],
-    install_requires=["pygame", "numpy", "numba"],
-    ext_modules=cythonize("robots/engine_c/engine.pyx", language_level="3", annotate=True),
+    install_requires=["pygame"],
     include_package_data=True,
     zip_safe=False,
 )
